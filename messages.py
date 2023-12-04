60b8fca48823c26b33a1baf45955e00d48b773bf
teome <domkelly1010@gmail.com>
2023-12-04 17:29:09 +0000

    Clean up and make importable

--- messages.py
+++ messages.py
@@ -1,9 +1,8 @@
-# %%
 import json
-import requests
-import openai
+from typing import NamedTuple
+from typing import TypedDict, Required, Literal
+from pydantic import BaseModel
 
-# %%
 
 class BColors:
     HEADER = '\033[95m'
@@ -30,14 +29,9 @@ terminal_colors = {
 }
 
 
-# %%
-# %%[markdown]
 # Helper message class, with and without pydantic
 
-# Light version without pydantic
-from typing import NamedTuple
-from pydantic import BaseModel
-
+# Light version without pydantic but named tuple
 class MessageNT(NamedTuple):
     role: str
     content: str
@@ -47,10 +41,16 @@ class MessagePydantic(BaseModel):
     role: str
     content: str
 
-Message = MessagePydantic
+# Use TypedDict as the openai python lib does and keep it simple
+Role = Literal["system", "user", "assistant", "function"]
 
-# %%[markdown]
-# ## Conversation class to wrap up a list of messages for interactiona
+class MessageTD(TypedDict, total=False):
+    role: Required[Role]
+    content: Required[str]
+
+Message = MessageTD
+
+# Conversation class to wrap up a list of messages for interaction
 class Conversation(list):
     def __init__(self, messages=None, color_scheme=None):
         super().__init__(messages or [])
@@ -61,51 +61,63 @@ class Conversation(list):
             "function": "cyan",
         }
 
-    def add_message(self, role: str, content: str):
+    def add_message(self, role: Role, content: str):
         """Add a message to the conversation."""
         self.append(Message(role=role, content=content))
 
     def display_conversation(self, detailed=False):
         """Display the conversation."""
         for message in self:
-            terminal_color = terminal_colors[self.color_scheme.get(message.role, "white")]
-            print(f"{terminal_color}{message.role}: {message.content}{terminal_colors['endc']}")
+            terminal_color = terminal_colors[self.color_scheme.get(message["role"], "white")]
+            if detailed:
+                non_role_strs = ' |'.join([f"{k}: {v}" for k, v in message.items() if k != 'role'])
+                print(f"{terminal_color}{message['role']}: {non_role_strs}{terminal_colors['endc']}")
+            else:
+                print(f"{terminal_color}{message['role']}: {message['content']}{terminal_colors['endc']}")
 
     def delete_interaction(self, index=-1):
         """Delete an interaction from the conversation."""
         if index == -1:
             index = len(self) - 2
 
-        if index < 0 or index >= len(self) or self[index].role != 'user':
+        if index < 0 or index >= len(self) or self[index]["role"] != 'user':
             raise RuntimeError("Invalid index for deletion. Index does not correspond to a user message.")
 
-        if index + 1 >= len(self) or self[index + 1].role != 'assistant':
+        if index + 1 >= len(self) or self[index + 1]["role"] != 'assistant':
             raise RuntimeError("Invalid interaction. No assistant message following the user message.")
 
         del self[index:index + 2]
 
+    def json(self):
+        return json.dumps(self)
 
-# %%
-# %%
-messages=[{
-    "role": "system",
-    "content": "You are a helpful assistant."
-}, {
-    "role": "user",
-    "content": "Who won the FA Cup in 2022?"
-}, {
-    "role": "assistant",
-    "content": "Liverpool won the FA Cup in 2022."
-},
-]
-messages = [Message(**x) for x in messages]
-print(messages)
 
-# %%
-# print(messages)
-# messages = [Message(role="system", content="You are a helpful assistant."),
-#             Message(role="user", content="Who were the members of drexciya?")]
-conversation = Conversation(messages=messages)
-conversation.display_conversation()
+if __name__ == "__main__":
+    messages=[{
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }, {
+        "role": "user",
+        "content": "Who won the FA Cup in 2022?"
+    }, {
+        "role": "assistant",
+        "content": "Liverpool won the FA Cup in 2022."
+    },
+    ]
 
-# %%
+    conversation = Conversation(messages=messages)
+    conversation.display_conversation()
+    conversation.display_conversation(detailed=True)
+    print(conversation.json())
+    print(conversation)
+
+    conversation.delete_interaction()
+    conversation.display_conversation()
+    conversation.add_message(role="user", content="Who won the FA Cup in 2022?")
+    conversation.add_message(role="assistant", content="Liverpool won the FA Cup in 2022.")
+    conversation.delete_interaction()
+    conversation.display_conversation()
+
+    conversation.append({"role": "user", "content": "Who won the FA Cup in 2022?"})
+    conversation.append({"role": "assistant", "content": "Liverpool won the FA Cup in 2022."})
+    conversation.display_conversation(detailed=True)
