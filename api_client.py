7e2e459a2a2403e5fcee9bb1361e4d9e437208a8
teome <domkelly1010@gmail.com>
2023-12-22 11:19:52 +0000

    Update API client to handle streaming tokens: only tested working for together.ai

--- api_client.py
+++ api_client.py
@@ -1,6 +1,8 @@
+import json
 import os
 from typing import Optional
 from fire import Fire
+import sseclient
 
 from endpoint_utils import rest_api_request, get_response, iterate_streaming_response
 
@@ -10,7 +12,7 @@ def main(
         url: str = "https://api.together.xyz/inference",
         api_key: Optional[str] = None,
         prompt: str = "Explain entropy in terms of information theory.",
-        model="mistralai/Mixtral-8x7B-v0.1",
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         temperature=0.8,
         max_tokens=1024,
         repetition_penalty=1.2,
@@ -27,19 +29,25 @@ def main(
 
     if "together" in url or "vllm" in url:
         json_data = {
-            "prompt": prompt,
             "model": model,
             "temperature": temperature,
             "max_tokens": max_tokens,
             "repetition_penalty": repetition_penalty,
+            "stream_tokens": stream,
         }
+        if "mistral" in model and "Instruct" in model:
+            json_data["prompt"] = f"[INST] {prompt} [/INST]"
+            json_data["stop"] = ["</s>", "[/INST]"]
+        else:
+            json_data["prompt"] = prompt
     elif "openai" in url:
         json_data = {
             "messages": [{"role": "user", "content": prompt},],
             "model": model,
             "temperature": temperature,
             "max_tokens": max_tokens,
-            "repetition_penalty": repetition_penalty,
+            "frequency_penalty": repetition_penalty,
+            "stream": stream,
         }
 
     response = rest_api_request(
@@ -49,22 +57,17 @@ def main(
         stream=stream,
     )
 
-    def extract_message(response):
-        if "together" in url or "vllm" in url:
-            return response["output"]["choices"][0]["text"]
-        elif "openai" in url:
-            return response["choices"][0]["message"]["content"]
-        else:
-            raise NotImplementedError("No handling available for this url")
+    if "together" in url or "vllm" in url:
+        client = sseclient.SSEClient(response)
+        for event in client.events():
+            if event.data == "[DONE]":
+                break
 
-
-    if stream:
-        for i, response in enumerate(iterate_streaming_response(response)):
-            print(f"\n\nResponse {i}:")
-            print(extract_message(response), flush=True)
-    else:
-        response = get_response(response)
-        print(extract_message(response))
+            partial_result = json.loads(event.data)
+            token = partial_result["choices"][0]["text"]
+            print(token, end="", flush=True)
+    elif "openai" in url:
+        raise NotImplementedError("OpenAI streaming not implemented")
 
 
 if __name__ == '__main__':
