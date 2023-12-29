a4c8592cbb23902a56d0e0fbcb422d9450f95319
teome <domkelly1010@gmail.com>
2023-12-29 12:57:28 +0000

    Make compatible with other endpoints

--- ui/server_openai_api.py
+++ ui/server_openai_api.py
@@ -8,6 +8,20 @@ import gradio as gr
 # from ..endpoint_utils import retry_on_failure
 
 DEFAULT_MODEL = "gpt-4"
+MODEL_LIST = [
+    "gpt-3", "gpt-3.5-turbo",
+    "gpt-4", "gpt-4-1106-preview", "gpt-4-0613", "gpt-4-0314",
+    "mistralai/Mistral-7B-Instruct-v0.2", "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    "togethercomputer/llama-2-70b-chat",
+    "zero-one-ai/Yi-34B-Chat",
+]
+
+SYSTEM_PROMPT = """\
+You are a helpful, inciteful, accurate and up to date pair programmer. \
+When beginning a response, you outline each step and the reasoning behind it, for the problem or task to be solved. Having done this, you should propose a solution. \
+The solution can be in the form of code, which should be in codeblocks, or in the form of a plan or system description as appropriate.
+"""
+
 
 try:
     api_key = os.environ["OPENAI_API_KEY"]
@@ -19,23 +33,20 @@ except KeyError:
 if api_key is None:
     raise RuntimeError("OPENAI_API_KEY not set")
 
-global client
-client: Optional[openai.OpenAI] = None
+client: openai.OpenAI
+
 
 def chat_completions_create(
     messages: List[openai.types.chat.ChatCompletionMessageParam],
+    client: openai.OpenAI,
     model: str = DEFAULT_MODEL,
     max_tokens: int = 150,
     temperature: float = 0.8,
-    client: Optional[openai.OpenAI] = client,
     stream: bool = True,
     max_retries: int = 2,
     timeout: int = 60,
     **kwargs):
     """Create chat completions"""
-    if client is None:
-        # Create an instance of the OpenAI class, assumes OPENAI_API_KEY is set
-        client = openai.OpenAI(api_key=api_key, max_retries=max_retries, timeout=timeout)
 
     # @retry_on_failure(
     #     max_retries=max_retries,
@@ -61,7 +72,7 @@ def chat_completions_create(
     return _call()
 
 
-def predict(message, history, model=DEFAULT_MODEL, system_prompt="", max_tokens=1024, temperature=0.8):
+def predict(message, history, model=DEFAULT_MODEL, system_prompt=SYSTEM_PROMPT, max_tokens=1024, temperature=0.8):
     history_openai_format = []
     if system_prompt:
         history_openai_format.append({"role": "system", "content": system_prompt})
@@ -70,9 +81,11 @@ def predict(message, history, model=DEFAULT_MODEL, system_prompt="", max_tokens=
         history_openai_format.append({"role": "assistant", "content":assistant})
     history_openai_format.append({"role": "user", "content": message})
 
+    # global client  # pylint: disable=global-statement
 
     response = chat_completions_create(
         messages=history_openai_format,
+        client=client,
         model=model,
         max_tokens=max_tokens,
         temperature=temperature,
@@ -91,10 +104,11 @@ def main(model=DEFAULT_MODEL, max_retries=2, timeout=60, base_url=None):
     client = openai.OpenAI(api_key=api_key, base_url=base_url, max_retries=max_retries, timeout=timeout)
 
     with gr.Blocks() as demo:
-        model  = gr.Dropdown([
-            "gpt-3", "gpt-3.5-turbo",
-            "gpt-4", "gpt-4-1106-preview", "gpt-4-0613", "gpt-4-0314"], label="Model")
-        system_prompt = gr.Textbox("You are helpful AI.", label="System Prompt")
+        model  = gr.Dropdown(MODEL_LIST,
+            value=model,
+            allow_custom_value=True,
+            label="Model")
+        system_prompt = gr.Textbox(SYSTEM_PROMPT, label="System Prompt")
         temperature = gr.Slider(minimum=0.0, maximum=1.0, step=0.1, value=0.8, label="Temperature")
         max_tokens = gr.Number(value=1024, label="Max Tokens")
 
